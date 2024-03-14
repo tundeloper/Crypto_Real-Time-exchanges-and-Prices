d73bfb3656b9637ed06d7436ec381ac8ab1de29f
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-03-14 23:36:47 +0100

    Updated

--- src/components/SelectCountary.jsx
+++ src/components/SelectCountary.jsx
@@ -2,16 +2,23 @@ import { useCountries } from "../hooks/Apis";
 import {Grid, Autocomplete, TextField} from "@mui/material"
 
 const SelectCountry = () => {
-    const [countriesData] = useCountries("https://restcountries.com/v3.1/all");
+    const [countriesData, error, loaded] = useCountries("https://restcountries.com/v3.1/all");
     
     console.log("countriesData, countriesData",countriesData,);
-
+    if(loaded){
+        return(
+            <div>Loading...</div>
+        )
+    }
+    if(error){
+        return(
+            <dive>Error: {error.message}</dive>
+        )
+    }
     return (
         <Grid item xs={12} md={3}>
             <Autocomplete
-                // options={countriesData.map((country) => country.name.common)}
-                options={["option1", "option2", "option3", "abdull"]}
-                // loading={loaded}
+                options={countriesData.map((country) => country.name.common)}
                 renderInput={(params) => <TextField {...params} label="Select Country" />}
             />
         </Grid>
