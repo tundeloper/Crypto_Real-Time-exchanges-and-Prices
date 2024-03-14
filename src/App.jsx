858cebc6c4a4e78c8656fc1d7f0cf53684b46e3e
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-03-14 19:20:13 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Button, Container, Typography, Grid} from "@mui/material"
+import InputeAmount from "./components/InputeAmount"
+import SelectCountary from "./components/SelectCountary"
+import SwitchCurrency from "./components/SwitchCurrency"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const StyleBox = {
+    background:"#fdfdfd",
+    marginTop: "10rem",
+    textAlign: "center",
+    color: "#222",
+    minHeight: "20rem",
+    borderRadius: 5,
+    padding: "4rem 2rem",
+    boxShadow: "0px 10px 15px -3px rgba(0, 0, 0, 0.1)",
+    position: "relative"
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="md" sx={StyleBox}>
+      {/* <Button>Click</Button> */}
+      <Typography variant= "h5" sx={{marginBottom: "2rem"}}>Stay Ahead with Accurate Conversions</Typography>
+      <Grid container spacing={2}>
+        <InputeAmount/>
+        <SelectCountary/>
+        <SwitchCurrency/>
+        <SelectCountary/>
+      </Grid>
+    </Container>
   )
 }
 
